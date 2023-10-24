f02971fd1bc12e5d8da20d15441c96db2a2cc3ae
Mathys97459 <mathys.dezitter@outlook.com>
2023-10-24 13:57:16 +0200

    first commit

---
+++ post-cree.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="poster.css">
+    <link rel="stylesheet" href="global.css">
+    <title>Document</title>
+
+    <nav role="navigation">
+        <div id="menuToggle">
+            <input type="checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu">
+                <a href="#hanchor-about">
+                    <li>À propos</li>
+                </a>
+                <a href="#hanchor-project">
+                    <li>Projets</li>
+                </a>
+                <a href="#hanchor-contact">
+                    <li>Contact</li>
+                </a>
+            </ul>
+        </div>
+    </nav>
+</head>
+<body>
+<?php
+    $host = "localhost"; //serveur
+    $username = "root"; //identifiant pour acceder à la base de données
+    $password = "root"; //mdp de username
+    $dbname = "Learning Lab"; //Nom de la base de données
+
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); //variable connexion : accès à la base de données
+        echo "ok";
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->GetMessage();
+    }
+
+
+    $sql = "INSERT INTO posts(posts_titre, posts_image, posts_user, posts_pp)
+    VALUES ('{$_POST['titre']}', '{$_POST['image']}','{$_POST['username']}', 'https://cours-informatique-gratuit.fr/wp-content/uploads/2014/05/compte-utilisateur-1.png')";
+    $conn->exec($sql);
+    ?>
+    <h2>Votre article à bien été ajouté !</h2>
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ poster.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $host = "localhost"; //serveur
+    $username = "root"; //identifiant pour acceder à la base de données
+    $password = "root"; //mdp de username
+    $dbname = "Learning Lab"; //Nom de la base de données
+
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); //variable connexion : accès à la base de données
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->GetMessage();
+    }
+
+
+    $affichage = "SELECT posts_id, posts_image, posts_titre, posts_pp, posts_user FROM posts";
+    $resultat = $conn->prepare($affichage);
+    $resultat->execute();
+    ?>
+
+<h2>Créer un post</h2>
+<form action="post-cree.php" method="post">
+    Titre du post: <input type="text" name="titre" placeholder="https://monimage.png">
+    Lien de votre image: <input type="text" name="image">
+    Nom d'utilisateur: <input type="text" name="username">
+    <input type="submit">
+</form>
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
